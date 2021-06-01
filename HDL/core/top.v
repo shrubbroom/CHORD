f4ecfedf60e444671fe585acb58de9856f749da1
shrubbroom <shrubbroom@sjtu.edu.cn>
2021-06-01 11:54:41 +0800

    add auto

--- HDL/core/top.v
+++ HDL/core/top.v
@@ -14,52 +14,41 @@ module top#(
             parameter SECTOR_FLAG_WIDTH = 2
             )(
               /*AUTOINPUT*/
-              // Beginning of automatic inputs (from unused autoinst inputs)
-              input                                   arctan_en_in, // To pipeline of pipeline.v
-              input                                   clk, // To pipeline of pipeline.v, ...
-              input signed [UNSIGNED_INPUT_WIDTH-1:0] degree_in,// To pipeline of pipeline.v, ...
-              input                                   reset, // To pipeline of pipeline.v
-              input                                   rst, // To interface_input of interface_input.v
-              input [SECTOR_FLAG_WIDTH-1:0]           sector_in,// To pipeline of pipeline.v
-              input                                   valid_in, // To pipeline of pipeline.v
-              input [UNSIGNED_INPUT_WIDTH-1:0]        x_in, // To pipeline of pipeline.v
-              input [UNSIGNED_INPUT_WIDTH-1:0]        y_in  // To pipeline of pipeline.v
-              // End of automatics
               );
    /*dummy top*/
    /*AUTOWIRE*/
    // Beginning of automatic wires (for undeclared instantiated-module outputs)
-   wire                                               arctan_en_out;          // From pipeline of pipeline.v
-   wire [UNSIGNED_OUTPUT_WIDTH-1:0]                   degree_out; // From pipeline of pipeline.v, ...
-   wire [1:0]                                         quadrant;               // From interface_input of interface_input.v
-   wire [SECTOR_FLAG_WIDTH-1:0]                       sector_out;     // From pipeline of pipeline.v
-   wire                                               valid_out;              // From pipeline of pipeline.v
-   wire [UNSIGNED_OUTPUT_WIDTH-1:0]                   x_out;      // From pipeline of pipeline.v
-   wire [UNSIGNED_OUTPUT_WIDTH-1:0]                   y_out;      // From pipeline of pipeline.v
+   wire			arctan_en_out;		// From pipeline of pipeline.v
+   wire [UNSIGNED_OUTPUT_WIDTH-1:0] degree_out;	// From pipeline of pipeline.v, ...
+   wire [1:0]		quadrant;		// From interface_input of interface_input.v
+   wire [SECTOR_FLAG_WIDTH-1:0] sector_out;	// From pipeline of pipeline.v
+   wire			valid_out;		// From pipeline of pipeline.v
+   wire [UNSIGNED_OUTPUT_WIDTH-1:0] x_out;	// From pipeline of pipeline.v
+   wire [UNSIGNED_OUTPUT_WIDTH-1:0] y_out;	// From pipeline of pipeline.v
    // End of automatics
    pipeline pipeline(/*AUTOINST*/
-                     // Outputs
-                     .degree_out        (degree_out[UNSIGNED_OUTPUT_WIDTH-1:0]),
-                     .x_out             (x_out[UNSIGNED_OUTPUT_WIDTH-1:0]),
-                     .y_out             (y_out[UNSIGNED_OUTPUT_WIDTH-1:0]),
-                     .sector_out        (sector_out[SECTOR_FLAG_WIDTH-1:0]),
-                     .arctan_en_out     (arctan_en_out),
-                     .valid_out         (valid_out),
-                     // Inputs
-                     .clk               (clk),
-                     .reset             (reset),
-                     .degree_in         (degree_in[UNSIGNED_INPUT_WIDTH-1:0]),
-                     .x_in              (x_in[UNSIGNED_INPUT_WIDTH-1:0]),
-                     .y_in              (y_in[UNSIGNED_INPUT_WIDTH-1:0]),
-                     .sector_in         (sector_in[SECTOR_FLAG_WIDTH-1:0]),
-                     .arctan_en_in      (arctan_en_in),
-                     .valid_in          (valid_in));
+		     // Outputs
+		     .degree_out	(degree_out[UNSIGNED_OUTPUT_WIDTH-1:0]),
+		     .x_out		(x_out[UNSIGNED_OUTPUT_WIDTH-1:0]),
+		     .y_out		(y_out[UNSIGNED_OUTPUT_WIDTH-1:0]),
+		     .sector_out	(sector_out[SECTOR_FLAG_WIDTH-1:0]),
+		     .arctan_en_out	(arctan_en_out),
+		     .valid_out		(valid_out),
+		     // Inputs
+		     .clk		(clk),
+		     .reset		(reset),
+		     .degree_in		(degree_in[UNSIGNED_INPUT_WIDTH-1:0]),
+		     .x_in		(x_in[UNSIGNED_INPUT_WIDTH-1:0]),
+		     .y_in		(y_in[UNSIGNED_INPUT_WIDTH-1:0]),
+		     .sector_in		(sector_in[SECTOR_FLAG_WIDTH-1:0]),
+		     .arctan_en_in	(arctan_en_in),
+		     .valid_in		(valid_in));
    interface_input interface_input(/*AUTOINST*/
-                                   // Outputs
-                                   .degree_out          (degree_out[UNSIGNED_OUTPUT_WIDTH-1:0]),
-                                   .quadrant            (quadrant[1:0]),
-                                   // Inputs
-                                   .clk                 (clk),
-                                   .rst                 (rst),
-                                   .degree_in           (degree_in[UNSIGNED_INPUT_WIDTH-1:0]));
+				   // Outputs
+				   .degree_out		(degree_out[UNSIGNED_OUTPUT_WIDTH-1:0]),
+				   .quadrant		(quadrant[1:0]),
+				   // Inputs
+				   .clk			(clk),
+				   .rst			(rst),
+				   .degree_in		(degree_in[UNSIGNED_INPUT_WIDTH-1:0]));
 endmodule // top
