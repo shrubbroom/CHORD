5cab763f43ae39ae48a74a9c0e19f203490cf2ca
shrubbroom <shrubbroom@sjtu.edu.cn>
2021-06-22 20:49:36 +0800

    changed chord_top

--- HDL/chord_top.v
+++ HDL/chord_top.v
@@ -1,62 +1,47 @@
 `include "ahb_lite_cordic.v"
 `include "fifo.v"
-`include "../core/ex_top.v"
+`include "./core/ex_top.v"
 module chord_top(
-                 /*AUTOINPUT*/
-                 // Beginning of automatic inputs (from unused autoinst inputs)
-                 input [31:0]    HADDR,                  // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input [2:0]     HBURST,                 // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input           HCLK,                   // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input           HMASTLOCK,              // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input [3:0]     HPROT,                  // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input           HREADY,                 // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input           HRESETn,                // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input           HSEL,                   // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input [2:0]     HSIZE,                  // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input [1:0]     HTRANS,                 // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input [31:0]    HWDATA,                 // To ahb_lite_cordic of ahb_lite_cordic.v
-                 input           HWRITE,                 // To ahb_lite_cordic of ahb_lite_cordic.v
-                 // End of automatics
-                 /*AUTOOUTPUT*/
-                 // Beginning of automatic outputs (from unused autoinst outputs)
-                 output [31:0]   HRDATA,                 // From ahb_lite_cordic of ahb_lite_cordic.v
-                 output          HREADYOUT,              // From ahb_lite_cordic of ahb_lite_cordic.v
-                 output          HRESP                  // From ahb_lite_cordic of ahb_lite_cordic.v
-                 // End of automatics
+                 input [31:0]    HADDR,
+                 input [2:0]     HBURST,
+                 input           HCLK,
+                 input           HMASTLOCK,
+                 input [3:0]     HPROT,
+                 input           HREADY,
+                 input           HRESETn,
+                 input           HSEL,
+                 input [2:0]     HSIZE,
+                 input [1:0]     HTRANS,
+                 input [31:0]    HWDATA,
+                 input           HWRITE,
+                 output [31:0]   HRDATA,
+                 output          HREADYOUT,
+                 output          HRESP
                  );
-   wire                        clk;
-   wire                        reset;
+   wire                          clk;
+   wire                          reset;
    assign clk = HCLK;
    assign reset = HRESETn;
-   /*AUTOWIRE*/
-   // Beginning of automatic wires (for undeclared instantiated-module outputs)
-   wire                 empty;                  // From fifo of fifo.v
-   wire [31:0]          in_interface;           // From ahb_lite_cordic of ahb_lite_cordic.v
-   wire [31:0]          out_fifo;               // From fifo of fifo.v
-   wire [31:0]          out_interface;          // From ex_top of ex_top.v
-   wire                 read_fifo_en;           // From ahb_lite_cordic of ahb_lite_cordic.v
-   wire                 valid_in_interface;     // From ahb_lite_cordic of ahb_lite_cordic.v
-   wire                 valid_out_interface;    // From ex_top of ex_top.v
-   // End of automatics
-   fifo fifo(/*AUTOINST*/
-             // Outputs
-             .out_fifo                  (out_fifo[31:0]),
+   wire                          empty;
+   wire [31:0]                   in_interface;
+   wire [31:0]                   out_fifo;
+   wire [31:0]                   out_interface;
+   wire                          read_fifo_en;
+   wire                          valid_in_interface;
+   wire                          valid_out_interface;
+   fifo fifo(.out_fifo                  (out_fifo[31:0]),
              .empty                     (empty),
-             // Inputs
              .clk                       (clk),
              .reset                     (reset),
              .read_fifo_en              (read_fifo_en),
              .valid_out_interface       (valid_out_interface),
              .out_interface             (out_interface[31:0]));
-   ahb_lite_cordic ahb_lite_cordic(/*AUTOINST*/
-                                   // Outputs
-                                   .HREADYOUT           (HREADYOUT),
+   ahb_lite_cordic ahb_lite_cordic(.HREADYOUT           (HREADYOUT),
                                    .HRESP               (HRESP),
                                    .HRDATA              (HRDATA[31:0]),
                                    .in_interface        (in_interface[31:0]),
                                    .valid_in_interface  (valid_in_interface),
                                    .read_fifo_en        (read_fifo_en),
-                                   // Inputs
                                    .HSEL                (HSEL),
                                    .HCLK                (HCLK),
                                    .HRESETn             (HRESETn),
@@ -72,11 +57,8 @@ module chord_top(
                                    .valid_out_interface (valid_out_interface),
                                    .out_fifo            (out_fifo[31:0]),
                                    .empty               (empty));
-   ex_top ex_top(/*AUTOINST*/
-                 // Outputs
-                 .out_interface         (out_interface[31:0]),
+   ex_top ex_top(.out_interface         (out_interface[31:0]),
                  .valid_out_interface   (valid_out_interface),
-                 // Inputs
                  .clk                   (clk),
                  .in_interface          (in_interface[31:0]),
                  .reset                 (reset),
